ed84a9e53e4f4541eaa7a0f00a9339224018f76e
Aleksejs Jurenoks <40733171+ajurenoks@users.noreply.github.com>
2023-08-30 13:50:11 +0300

    Update source.py

--- source.py
+++ source.py
@@ -1,6 +1,6 @@
 def main():
-    dollars = dollars_to_float(input("How much was the meal? ").replace("\n\r"),"")
-    percent = percent_to_float(input("What percentage would you like to tip? ").replace("\n\r",""))
+    dollars = dollars_to_float(input("How much was the meal? ").rstrip())
+    percent = percent_to_float(input("What percentage would you like to tip? ").rstrip())
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
